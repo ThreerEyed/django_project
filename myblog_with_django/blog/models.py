6a3109b1ab950ccf9256b62a1708bd2ac4de7c7b
qiang <2622509069@qq.com>
2018-07-18 20:58:15 +0800

    blog

--- myblog_with_django/blog/models.py
+++ myblog_with_django/blog/models.py
@@ -1,7 +1,13 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # blog 目录
+from django.urls import reverse
+
+
 class Category(models.Model):
     """
     Category 只需要一个简单的分类名 name 就可以了。
@@ -35,6 +41,36 @@ class Blog(models.Model):
     5. 文章摘要
     6. 各种关联字段
     """
+    # 标题
+    title = models.CharField(max_length=100)
+    # 创建时间
+    create_time = models.DateTimeField(default=datetime.now())
+    # modified 修改时间
+    modified = models.DateTimeField(default=datetime.now())
+    # 文章正文
+    content = models.TextField()
+    # 总共阅读数量
+    count = models.PositiveIntegerField(default=0)
+    # 文章摘要或者文章在前
+    excerpt = models.CharField(max_length=300, blank=True)
+
+    # 外键关联字段, 目录和文章的关系是一对多的关系，所以这里我们采取一对多关联模式
+    category = models.ForeignKey(Category)
+    # 一个标签对应多个文章，一个文章也可以有多个标签，所以这里采用多对多的模式
+    tag = models.ManyToManyField(Tag, blank=True)
+
+    # 作者关联，这里我们采用的也是一对多的关联模式，一篇文章的作者通常只有一个，一个作者却可以有多篇文章
+    # 这里的user 我们使用的是django 自带的user， 里面有这么几个字段
+    # Username, password and email are required. Other fields are optional.
+    author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('')
+
+
 
 
 
