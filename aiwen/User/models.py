f99cc628339530cd1620fcea104dd367fa6f6794
ThreeREyeD <2622509069@qq.com>
2018-07-26 00:12:46 +0800

    建立用户模型

--- aiwen/User/models.py
+++ aiwen/User/models.py
@@ -1,3 +1,36 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    """定义基础模型 用于继承"""
+    create_time = models.DateTimeField(default=datetime.now())
+    update_time = models.DateTimeField(default=datetime.now())
+
+
+class User(Base):
+    """
+    定义 用户模型 用来存储用户的相关信息
+    """
+    # 用户id
+    user_id = models.IntegerField(primary_key=True, auto_created=True, unique=True)
+    # 用户名
+    name = models.CharField(max_length=30, unique=True, null=False)
+    # 用户手机号
+    phone = models.CharField(max_length=11, unique=True, null=False)
+    # 用户密码
+    password = models.CharField(max_length=255, null=False)
+    # 用户邮箱
+    email = models.CharField(max_length=255, null=True)
+    # 昵称
+    nickname = models.CharField(max_length=30, null=True)
+    # 个人头像
+    avatar = models.CharField(max_length=300, null=True)
+    # 个人签名
+    self_introduction = models.CharField(max_length=50, null=True)
+
+    class Meta:
+        db_table = 'user_tb'
+
+
--- aiwen/aiwen/settings.py
+++ aiwen/aiwen/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'User',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'aiwen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aiwen_db',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': '123456',
     }
 }
 
